8d880817a52f45a4881dc6ff4b644b2e24d707e4
Ashlyn Knox <ashlyn@lilyx.io>
2024-02-22 11:28:43 -0700

    brute force implementation

--- main.js
+++ main.js
@@ -3,12 +3,38 @@
 // DONE: Set up array of words to test carousel logic
 const carouselCollection = ["Bard", "Rogue", "Paladin", "Theif"]
 
-// TODO: get elements from dom
+// DONE: get elements from dom
+const carouselContent = document.querySelector("#carousel-content")
+const prevBtn = document.querySelector("#prev")
+const nextBtn = document.querySelector("#next")
 
+let currentSlide = 0
+const maxLength = carouselCollection.length - 1;
+// DONE: show first element in the array by default
+carouselContent.innerHTML = `<h2>${carouselCollection[currentSlide]}</h2>`
 
-// TODO: add next logic
+// DONE: add next logic
+function getNextItem() {
+  if (currentSlide === maxLength) {
+    currentSlide = 0;
+    carouselContent.innerHTML = `<h2>${carouselCollection[currentSlide]}</h2>`
+  } else {
+    currentSlide++
+    carouselContent.innerHTML = `<h2>${carouselCollection[currentSlide]}</h2>`
+  }
+}
 
-// TODO: add previous logic
-
-// TODO: set event listeners
+// DONE: add previous logic
+function getPrevItem() {
+  if (currentSlide === 0) {
+    currentSlide = carouselCollection.length;
+    carouselContent.innerHTML = `<h2>${carouselCollection[currentSlide]}</h2>`
+  } else {
+    currentSlide -= 1
+    carouselContent.innerHTML = `<h2>${carouselCollection[currentSlide]}</h2>`
+  }
+}
+// DONE: set event listeners
+nextBtn.addEventListener("click", getNextItem)
+prevBtn.addEventListener("click", getPrevItem)
 
