5b59ed997f097457ed2d8354417f19cd4db19902
Ashlyn Knox <ashlyn@lilyx.io>
2024-02-22 10:24:59 -0700

    carousel setup

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 "use strict";
+
+// DONE: Set up array of words to test carousel logic
+const carouselCollection = ["Bard", "Rogue", "Paladin", "Theif"]
+
+// TODO: get elements from dom
+
+
+// TODO: add next logic
+
+// TODO: add previous logic
+
+// TODO: set event listeners
+
